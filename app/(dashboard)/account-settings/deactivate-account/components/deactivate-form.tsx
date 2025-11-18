309988107a040d89273016559c7b80f29091826c
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-18 15:07:52 +0600

    confirm deactivation done

--- app/(dashboard)/account-settings/deactivate-account/components/deactivate-form.tsx
+++ app/(dashboard)/account-settings/deactivate-account/components/deactivate-form.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import ConfirmDeactivation from "./confirm-deactivation";
 import DeactivateReason from "./deactivate-reason";
 
 const DeactivateForm = ({ token }: { token: string }) => {
   return (
     <div className="mt-8 space-y-8">
       <DeactivateReason token={token} />
+      <ConfirmDeactivation token={token} />
     </div>
   );
 };
